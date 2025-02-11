91654e0d94846362ee0327ea265ebb61b50c502a
Benjamin Hamon <development@benjaminhamon.com>
2025-02-11 17:36:09 +0100

    Add Unity editor client and helpers

---
+++ Automation/Scripts/automation_scripts/toolkit/unity/unity_editor_client.py
@@ -0,0 +1,119 @@
+import logging
+import os
+import subprocess
+import sys
+from typing import List, Optional
+
+from bhamon_development_toolkit.processes import process_helpers
+from bhamon_development_toolkit.processes.exceptions.process_exception import ProcessException
+from bhamon_development_toolkit.processes.exceptions.process_start_exception import ProcessStartException
+from bhamon_development_toolkit.processes.executable_command import ExecutableCommand
+from bhamon_development_toolkit.processes.process_options import ProcessOptions
+from bhamon_development_toolkit.processes.process_output_handler import ProcessOutputHandler
+from bhamon_development_toolkit.processes.process_output_logger import ProcessOutputLogger
+from bhamon_development_toolkit.processes.process_runner import ProcessRunner
+
+from automation_scripts.toolkit.unity.unity_editor_arguments import UnityEditorArguments
+
+logger = logging.getLogger("Unity")
+
+
+class UnityEditorClient:
+
+
+    def __init__(self, process_runner: ProcessRunner, unity_executable: str, project_path: str) -> None:
+        self._process_runner = process_runner
+        self._unity_executable = unity_executable
+        self._project_path = project_path
+
+
+    def launch(self, log_file_path: Optional[str] = None, simulate: bool = False) -> None:
+        command = ExecutableCommand(self._unity_executable)
+        command.add_arguments([ "-projectPath", self._project_path ])
+        command.add_internal_arguments([ "-logFile", os.path.abspath(log_file_path) ] if log_file_path is not None else [], [])
+
+        logger.info("Launching editor (Project: '%s')", self._project_path)
+        logger.debug("+ %s", process_helpers.format_executable_command(command.get_command_for_logging()))
+
+        if not simulate:
+            try:
+                subprocess.Popen(command.get_command()) # pylint: disable = consider-using-with
+            except FileNotFoundError as exception:
+                exception_message = "Executable not found: '%s'" % (command.executable_name)
+                raise ProcessStartException(exception_message, command.executable_path, None) from exception
+
+
+    async def run(self,
+            unity_arguments: UnityEditorArguments, custom_arguments: Optional[List[str]] = None,
+            log_file_path: Optional[str] = None, simulate: bool = False) -> None:
+
+        await self._run_internal(unity_arguments, custom_arguments, log_file_path = log_file_path, simulate = simulate)
+
+
+    async def reimport(self,
+            build_target: Optional[str] = None, enable_graphics = False,
+            log_file_path: Optional[str] = None, simulate: bool = False) -> None:
+
+        unity_arguments = UnityEditorArguments(
+            batch_mode = True,
+            enable_graphics = enable_graphics,
+            quit_on_completion = True,
+            build_target = build_target)
+
+        logger.info("Reimporting (Project: '%s')", self._project_path)
+
+        try:
+            await self._run_internal(unity_arguments, log_file_path = log_file_path, simulate = simulate)
+        except ProcessException as exception:
+            raise RuntimeError("Reimport failed") from exception
+
+
+    async def execute(self, # pylint: disable = too-many-arguments
+            unity_arguments: UnityEditorArguments, method: str, method_arguments: Optional[List[str]] = None,
+            log_file_path: Optional[str] = None, simulate: bool = False) -> None:
+
+        custom_arguments = [ "-executeMethod", method ]
+        if method_arguments is not None:
+            custom_arguments += method_arguments
+
+        logger.info("Executing '%s' (Project: '%s')", method, self._project_path)
+
+        try:
+            await self._run_internal(unity_arguments, custom_arguments, log_file_path = log_file_path, simulate = simulate)
+        except ProcessException as exception:
+            raise RuntimeError("Unity execute failed") from exception
+
+
+    async def _run_internal(self,
+            unity_arguments: UnityEditorArguments, custom_arguments: Optional[List[str]] = None,
+            log_file_path: Optional[str] = None, simulate: bool = False) -> None:
+
+        command_as_object = ExecutableCommand(self._unity_executable)
+        command_as_object.add_arguments([ "-projectPath", self._project_path ])
+
+        command_as_object.add_arguments([ "-batchMode" ] if unity_arguments.batch_mode else [])
+        command_as_object.add_arguments([ "-noGraphics" ] if not unity_arguments.enable_graphics else [])
+        command_as_object.add_arguments([ "-quit" ] if unity_arguments.quit_on_completion else [])
+        command_as_object.add_arguments([ "-buildTarget", unity_arguments.build_target ] if unity_arguments.build_target is not None else [])
+        command_as_object.add_internal_arguments([ "-logFile", "-" ], [])
+
+        if custom_arguments is not None:
+            command_as_object.add_arguments(custom_arguments)
+
+        process_options = ProcessOptions()
+        raw_logger = process_helpers.create_raw_logger(log_file_path = log_file_path)
+        raw_logger.configure_log_stream(sys.stdout, "debug")
+        process_output_logger = ProcessOutputLogger(raw_logger.get_actual_logger())
+        output_handlers: List[ProcessOutputHandler] = [ process_output_logger ]
+
+        logger.info("Running editor (Project: '%s')", self._project_path)
+        logger.debug("+ %s", process_helpers.format_executable_command(command_as_object.get_command_for_logging()))
+
+        try:
+            if not simulate:
+                try:
+                    await self._process_runner.run(command_as_object, process_options, output_handlers, check_exit_code = True)
+                finally:
+                    logger.debug("Process log file: '%s'", log_file_path)
+        finally:
+            raw_logger.dispose()
